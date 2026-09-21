7abbc8ca967ad8985273f6f455b61e028fd2b59a
hendriwrb <hendriwrb2022@gmail.com>
2026-09-21 20:17:13 +0000

    Mais um item

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,19 @@ app.use(express.json());
 app.use(express.static("../frontend", { index: "index.html" }));
 
 app.get("/produto", (req, res) => {
-    const produto = {
+    const produto = [
+        {
         id: 1,
         nome: "Melancia",
         preco: 23,
         categoria: "Alimento"
-    };
+        },
+        {
+        id: 2,
+        nome: "Amaciante downy",
+        preco: 27,
+        categoria: 'Produto de limpeza'
+        }];
 
     res.json(produto);
 });
